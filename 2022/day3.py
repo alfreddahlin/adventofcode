654b443f0934d288147752cae19e35a197e54aa2
Alfred Dahlin <dahlin.alfred@gmail.com>
2022-12-03 11:01:12 +0100

    Cleans up with list comprehension

--- 2022/day3.py
+++ 2022/day3.py
@@ -5,17 +5,13 @@ data = [
     ((line[: int(len(line) / 2)]), (line[int(len(line) / 2) :])) for line in input_data
 ]
 
-prios = []
-for pack in data:
-    prios_pack = []
-    for comp in pack:
-        prios_pack.append(
-            {
-                ord(item) - ord_a if ord(item) > ord_a else ord(item) - ord_A
-                for item in comp
-            }
-        )
-    prios.append(prios_pack)
+prios = [
+    [
+        {ord(item) - ord_a if ord(item) > ord_a else ord(item) - ord_A for item in comp}
+        for comp in pack
+    ]
+    for pack in data
+]
 
 print(
     "Part 1:",
@@ -24,12 +20,14 @@ print(
 
 badges = []
 prios_pack = [a.union(b) for a, b in prios]
-for i in range(0, len(prios_pack), 3):
+[
     badges.append(
         prios_pack[i]
         .intersection(prios_pack[i + 1])
         .intersection(prios_pack[i + 2])
         .pop()
     )
+    for i in range(0, len(prios_pack), 3)
+]
 
 print("Part 2:", sum(badges))

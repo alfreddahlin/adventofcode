df6264af63542d1fa679bc3abb4f0f788fa828ea
Alfred Dahlin <dahlin.alfred@gmail.com>
2022-12-05 10:05:20 +0100

    Simplifies template and ads initial run

--- day0.py
+++ day0.py
@@ -2,7 +2,8 @@ import re
 
 input_data = open("inputs/day0.in", "r").read().strip().split("\n")
 
+data = input_data
+# data = re.findall(r"\d+", input_data)
 # data = [re.findall(r'exp',line) for line in input_data]
-data = re.findall(r"\d+", input_data)
 
 print(data)

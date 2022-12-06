a738929bc07863e01fe160674b4c644f135c23d7
Alfred Dahlin <dahlin.alfred@gmail.com>
2022-12-06 06:35:30 +0100

    2022-12-06

---
+++ 2022/day6.py
@@ -0,0 +1,10 @@
+def find_set_index(e, n):
+    for i in range(n, len(e)):
+        if len(set(e[i - n : i])) == n:
+            return i
+
+
+input_data = open("inputs/day6.in", "r").read().strip()
+
+print("Part 1:", find_set_index(input_data, 4))
+print("Part 2:", find_set_index(input_data, 14))

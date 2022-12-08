c3e8a3a036b0728f3a6280bf6723b610b4fb53da
Alfred Dahlin <dahlin.alfred@gmail.com>
2022-12-08 15:52:16 +0100

    2022-12-10

---
+++ 2022/day10.py
@@ -0,0 +1,28 @@
+import re
+
+input_data = open("inputs/day10.in", "r").read().strip().split("\n")
+
+data = input_data
+ss_index = [40 * i + 20 for i in range(6)]
+reg_x = 1
+cycles = [1]
+
+for op in data:
+    cycles.append(reg_x)
+    if "addx" in op:
+        reg_x += int(op.split()[1])
+        cycles.append(reg_x)
+
+print("Part 1:", sum(cycles[i - 1] * i for i in ss_index))
+
+pix = ""
+for c, i in enumerate(cycles[:-1]):
+    sprite = c % 40
+    if sprite == 0:
+        pix += "\n"
+    if sprite >= i - 1 and sprite <= i + 1:
+        pix += "X"
+    else:
+        pix += "."
+
+print("Part 2:", pix)

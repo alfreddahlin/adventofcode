4f9016432582706c77aa97521c85732d1a828a91
Alfred Dahlin <dahlin.alfred@gmail.com>
2022-12-18 12:17:10 +0100

    Adds faster graph search for day 14

--- 2022/day14.py
+++ 2022/day14.py
@@ -1,4 +1,4 @@
-import re
+from copy import deepcopy
 
 input_data = open("inputs/day14.in", "r").read().strip().split("\n")
 
@@ -22,17 +22,42 @@ abyss = int(max([p.imag for p in cave]))
 p = source
 sands = 0
 p1 = None
-while source not in cave:
+sandy_cave = deepcopy(cave)
+while source not in sandy_cave:
     for f in fall:
-        if p + f not in cave and p.imag < abyss + 1:
+        if p + f not in sandy_cave and p.imag < abyss + 1:
             p += f
             break
     else:
         if p.imag >= abyss and p1 is None:
             p1 = sands
         sands += 1
-        cave.add(p)
+        sandy_cave.add(p)
         p = source
 
 print("Part 1:", p1)
 print("Part 2:", sands)
+
+# Faster BFS for part 2
+# def find_sands(data, start):
+#     sands = 0
+#     queue = [start]
+#     seen = {start}
+#     while queue:
+#         current = queue.pop(0)
+#         alternatives = get_neighbors(current)
+#         sands += 1
+#         queue += {
+#             new
+#             for new in alternatives
+#             if new not in seen and new not in data and new.imag <= abyss + 1
+#         }
+#         seen.update(alternatives)
+#     return sands
+
+
+# def get_neighbors(current):
+#     return {current + a for a in fall}
+
+
+# print("Part 2:", find_sands(cave, 500))
--- 2022/day18.py
+++ 2022/day18.py
@@ -2,8 +2,8 @@ input_data = open("inputs/day18.in", "r").read().strip().split("\n")
 adjacent = ((1, 0, 0), (-1, 0, 0), (0, 1, 0), (0, -1, 0), (0, 0, 1), (0, 0, -1))
 
 data = {tuple(int(c) for c in line.split(",")) for line in input_data}
-max_dim = max(max(c) for c in zip(*data))
 min_dim = min(min(c) for c in zip(*data))
+max_dim = max(max(c) for c in zip(*data))
 
 
 def find_surfaces(data, start):
@@ -12,7 +12,7 @@ def find_surfaces(data, start):
     seen = {start}
     while queue:
         current = queue.pop(0)
-        alternatives = get_alternatives(current)
+        alternatives = get_neighbors(current)
         surfaces += sum(1 for n in alternatives if n in data)
         queue += {
             new
@@ -25,16 +25,13 @@ def find_surfaces(data, start):
     return surfaces
 
 
-def get_alternatives(current):
+def get_neighbors(current):
     return {tuple(current[i] + a[i] for i in range(3)) for a in adjacent}
 
 
-neighbors = 0
+surfaces = 0
 for c in data:
-    neighbors += sum(tuple(c[i] + a[i] for i in range(3)) in data for a in adjacent)
+    surfaces += 6 - sum(n in data for n in get_neighbors(c))
 
-print("Part 1:", 6 * len(data) - neighbors)
-print(
-    "Part 2:",
-    find_surfaces(data, (0, 0, 0)),
-)
+print("Part 1:", surfaces)
+print("Part 2:", find_surfaces(data, (0, 0, 0)))
